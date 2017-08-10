c93bfa43ad360b05bcbc7d7ebc970a7faad6a731
Sanny Mulyono <smulyono@me.com>
2017-08-10 11:07:00 -0700

    [Type]  :sparkles:
    **Added**
    * progress during compilation
    * display of build and dev server information
    
    **Changed**
    * remove react_dev_utils dependency

--- packages/webpack-boot/scripts/start.js
+++ packages/webpack-boot/scripts/start.js
@@ -1,13 +1,9 @@
-const webpack = require("webpack"),
+const 
     webpackDevServer = require("webpack-dev-server"),
     chalk = require("chalk"),
     configBuilder = require("../config/configBuilder"),
+    util = require("../config/util"),
     constants = require("../config/constant");
-
-const {
-  createCompiler,
-  prepareUrls,
-} = require('react-dev-utils/WebpackDevServerUtils');
 
 console.log(chalk.blue("Webpack boot development server... [ x ]"));
 // set node_env as development
@@ -15,19 +11,19 @@ process.env.NODE_ENV = "development";
 
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 const appName = constants.PACKAGE_JSON.name;
-const urls = prepareUrls(protocol, constants.DEVELOPMENT_HOST, constants.DEVELOPMENT_PORT);
+const urls = util.getAddress(protocol, constants.DEVELOPMENT_HOST, constants.DEVELOPMENT_PORT);
 
 // configBuilder.getDevelopmentConfig();
 
 // Use react-dev-utils custom compiler which will give less verbose output for development
-let compiler = createCompiler(webpack, configBuilder.getDevelopmentConfig(), appName, urls, true);
+let compiler = util.compileWebpack(configBuilder.getDevelopmentConfig(), urls);
+
 let server = new webpackDevServer(compiler, constants.DEVELOPMENT_SERVER_CONFIG);
 server.listen(constants.DEVELOPMENT_PORT, constants.DEVELOPMENT_HOST, function (err) {
     if (err) {
         console.error("Unable to start development server");
         process.exit(1);
     }
-    console.log(chalk.green("Starting development running on http://" + constants.DEVELOPMENT_HOST + ":" + constants.DEVELOPMENT_PORT));
 });
 
 ['SIGINT', 'SIGTERM'].forEach(function(sig) {
